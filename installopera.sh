1b07cba0506460354f1c5435ba684ba7f24f37cc
freepikbaru <81853314+freepikbaru@users.noreply.github.com>
2021-04-03 16:43:35 +0700

    Create installopera.sh

---
+++ installopera.sh
@@ -0,0 +1,3 @@
+wget https://download3.operacdn.com/pub/opera/desktop/60.0.3255.27/linux/opera-stable_60.0.3255.27_amd64.deb
+sudo dpkg --install opera-stable_60.0.3255.27_amd64.deb
+sudo apt install --assume-yes --fix-broken
