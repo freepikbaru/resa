016494a962e8ac705ea2b757619f213bff702e69
freepikbaru <81853314+freepikbaru@users.noreply.github.com>
2021-04-03 17:08:46 +0700

    Update and rename installunzip.sh to install7z.sh

---
+++ install7z.sh
@@ -0,0 +1,3 @@
+wget http://ftp.br.debian.org/debian/pool/main/p/p7zip/p7zip-full_16.02+dfsg-8_amd64.deb
+sudo dpkg --install p7zip-full_16.02+dfsg-8_amd64.deb
+sudo apt install --assume-yes --fix-broken
