b1f6253300e56c0d4b4d1d2b470095fdc2dfacb3
freepikbaru <81853314+freepikbaru@users.noreply.github.com>
2021-04-03 15:59:29 +0700

    Update RDP2.sh
    hapus obs studio
    nambahin opera

--- RDP2.sh
+++ RDP2.sh
@@ -13,7 +13,6 @@ sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install obs-studio -y
 sudo apt install firefox -y
 sudo apt install falkon -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
